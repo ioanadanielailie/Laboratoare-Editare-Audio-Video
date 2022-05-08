1c08f4069544676fe9943ee52e116d157bad5af9
ioanadanielailie <ioanadaniela71200@gmail.com>
2022-05-08 19:52:28 +0300

    Laborator8

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(85, 160);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 354);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(560, 160);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(449, 354);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Convert to grayscale";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(314, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 64);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Generate histogram";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(776, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(883, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(740, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Alfa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(842, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Beta";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(464, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 64);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Change contrast";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1005, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(53, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(944, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Gamma";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(614, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 64);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Gamma correction";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(764, 13);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(144, 64);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Resize";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(914, 13);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(144, 64);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "Rotate";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 82);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(144, 64);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "Load video";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(193, 528);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(113, 22);
+            this.numericUpDown1.TabIndex = 17;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(162, 82);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(144, 64);
+            this.button9.TabIndex = 18;
+            this.button9.Text = "Read all frames";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(110, 530);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Frames";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(314, 82);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(144, 64);
+            this.button10.TabIndex = 20;
+            this.button10.Text = "Blend images";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(464, 82);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(144, 64);
+            this.button11.TabIndex = 21;
+            this.button11.Text = "Write to video";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 584);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,280 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> myImage;
+        Image<Bgr, byte> resizedImage;
+        Image<Bgr, byte> rotatedImage;
+        Image<Gray, byte> grayImage;
+        Image<Gray, byte> imgOutput;
+        Image<Bgr, byte> gammaOutput;
+        Rectangle rectangle;
+        Point startROI;
+        bool mouseDown;
+
+        int TotalFrame, FrameNo;
+        double Fps;
+        bool IsReadingFrame;
+        VideoCapture capture;
+        private static VideoCapture cameraCapture;
+        private Image<Bgr, Byte> newBackgroundImage;
+        private static IBackgroundSubtractor fgDetector;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFile = new OpenFileDialog ();
+            if(OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                myImage = new Image<Bgr, Byte>(OpenFile.FileName);
+                pictureBox1.Image = myImage.ToBitmap();
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            grayImage = myImage.Convert<Gray,Byte>();
+            pictureBox2.Image = grayImage.AsBitmap();
+            grayImage[0,0] = new Gray(200);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HistogramViewer v = new HistogramViewer();
+            v.HistogramCtrl.GenerateHistograms(myImage, 255);
+            v.Show();
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float alpha = float.Parse(textBox1.Text);
+            float beta = float.Parse(textBox2.Text);
+            imgOutput = new Image<Gray, Byte>(grayImage.Width, grayImage.Height);
+
+            for (int i = 0; i < grayImage.Height; i++)
+                for (int j = 0; j < grayImage.Width; j++)
+                {
+                    var formula = grayImage[i, j].Intensity * alpha + beta; 
+                    imgOutput[i,j] = new Gray(formula);
+                }
+            pictureBox2.Image = imgOutput.AsBitmap();
+        }
+
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            gammaOutput = myImage;
+            double gamma = double.Parse(textBox3.Text);
+            gammaOutput._GammaCorrect(gamma);
+            pictureBox2.Image = gammaOutput.AsBitmap();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            resizedImage = new Image<Bgr, byte>(myImage.Width, myImage.Height);
+            resizedImage = myImage.Resize(256,128,Emgu.CV.CvEnum.Inter.Cubic);
+            pictureBox2.Image = resizedImage.AsBitmap();
+        }
+   
+        private void button7_Click(object sender, EventArgs e)
+        {
+            rotatedImage = new Image<Bgr, byte>(myImage.Width, myImage.Height);
+            rotatedImage = myImage.Rotate(45,new Bgr(255,255,255));
+            pictureBox2.Image = rotatedImage.AsBitmap();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            startROI = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            int width = Math.Max(startROI.X, e.X) - Math.Min(startROI.X, e.X);
+            int height = Math.Max(startROI.Y, e.Y) - Math.Min(startROI.Y, e.Y);
+            rectangle = new Rectangle(
+                    Math.Min(startROI.X, e.X),
+                    Math.Min(startROI.Y, e.Y),
+                    width,
+                    height
+                );
+            int mouseX = e.X;
+            int mouseY = e.Y;
+            Refresh();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+            if (pictureBox1.Image == null || rectangle == Rectangle.Empty)
+                return;
+
+            var img = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+            img.ROI = rectangle;
+            var imgROI = img.Copy();
+            pictureBox2.Image = imgROI.ToBitmap();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                capture = new VideoCapture(ofd.FileName);
+                Mat m = new Mat();
+                capture.Read(m);
+                pictureBox1.Image = m.ToBitmap();
+
+                TotalFrame = (int)capture.Get(CapProp.FrameCount);
+                Fps = capture.Get(CapProp.Fps);
+                FrameNo = 1;
+                numericUpDown1.Value = FrameNo;
+                numericUpDown1.Minimum = 0;
+                numericUpDown1.Maximum = TotalFrame;
+
+            }
+
+        }
+
+        private async void ReadAllFrames()
+        {
+
+            Mat m = new Mat();
+            while (IsReadingFrame == true && FrameNo < TotalFrame)
+            {
+                FrameNo += 1;
+                var mat = capture.QueryFrame();
+                pictureBox1.Image = mat.ToBitmap();
+                await Task.Delay(1000 / Convert.ToInt16(Fps));
+                label4.Text = FrameNo.ToString() + "/" + TotalFrame.ToString();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    cameraCapture = new VideoCapture(@"D:\an3sem2\Editare audio-video\Do. Or do not. There is no try.mp4");
+            //    newBackgroundImage = new Image<Bgr, byte>(@"D:\an3sem2\Editare audio-video\lab-editare\lab-editare\Lab6\Untitled.jpg");
+            //    fgDetector = new BackgroundSubtractorMOG2();
+            //    Application.Idle += ProcessFrames;
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //    return;
+            //}
+            if (capture == null)
+            {
+                return;
+            }
+            IsReadingFrame = true;
+            ReadAllFrames();
+
+        }
+
+        private async void ProcessFrames(object sender, EventArgs e)
+        {
+            Mat frame = cameraCapture.QueryFrame();
+            Image<Bgr, byte> frameImage = frame.ToImage<Bgr, Byte>();
+
+            Mat foregroundMask = new Mat();
+            fgDetector.Apply(frame, foregroundMask);
+            var foregroundMaskImage = foregroundMask.ToImage<Gray, Byte>();
+            foregroundMaskImage = foregroundMaskImage.Not();
+
+            var copyOfNewBackgroundImage = newBackgroundImage.Resize(foregroundMaskImage.Width, foregroundMaskImage.Height, Inter.Lanczos4);
+            copyOfNewBackgroundImage = copyOfNewBackgroundImage.Copy(foregroundMaskImage);
+
+            foregroundMaskImage = foregroundMaskImage.Not();
+            frameImage = frameImage.Copy(foregroundMaskImage);
+            frameImage = frameImage.Or(copyOfNewBackgroundImage);
+        }
+
+        private async void button10_Click(object sender, EventArgs e)
+        {
+            string[] FileNames = Directory.GetFiles(@"C:\Users\ioana\Documents\GitHub\Laboratoare-Editare-Audio-Video\Lab6", "*.jpg");
+            List<Image<Bgr, byte>> listImages = new List<Image<Bgr, byte>>();
+            foreach (var file in FileNames)
+            {
+                listImages.Add(new Image<Bgr, byte>(file));
+            }
+            for (int i = 0; i < listImages.Count - 1; i++)
+            {
+                for (double alpha = 0.0; alpha <= 1.0; alpha += 0.01)
+                {
+                    pictureBox1.Image = listImages[i + 1].AddWeighted(listImages[i], alpha, 1 - alpha, 0).AsBitmap();
+                    await Task.Delay(25);
+                }
+            }
+
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            VideoCapture capture = new VideoCapture(@"C:\Users\ioana\Documents\GitHub\Laboratoare-Editare-Audio-Video\Lab6\video.mpg");
+
+            int Fourcc = Convert.ToInt32(capture.Get(CapProp.FourCC));
+            int Width = Convert.ToInt32(capture.Get(CapProp.FrameWidth));
+            int Height = Convert.ToInt32(capture.Get(CapProp.FrameHeight));
+            var Fps = capture.Get(CapProp.Fps);
+            var TotalFrame = capture.Get(CapProp.FrameCount);
+
+
+            string destionpath = @"C:\Users\ioana\Documents\GitHub\Laboratoare-Editare-Audio-Video\Lab6\ouput.mpg";
+            using (VideoWriter writer = new VideoWriter(destionpath, Fourcc, Fps, new Size(Width, Height), true))
+            {
+                Image<Bgr, byte> logo = new Image<Bgr, byte>(@"C:\Users\ioana\Documents\GitHub\Laboratoare-Editare-Audio-Video\Lab6\logo.jpg");
+                Mat m = new Mat();
+
+                var FrameNo = 1;
+                while (FrameNo < TotalFrame)
+                {
+                    capture.Read(m);
+                    Image<Bgr, byte> img = m.ToImage<Bgr, byte>();
+                    img.ROI = new Rectangle(Width - logo.Width - 30, 10, logo.Width, logo.Height);
+                    logo.CopyTo(img);
+
+                    img.ROI = Rectangle.Empty;
+
+                    writer.Write(img.Mat);
+                    FrameNo++;
+                }
+            }
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (mouseDown)
+            {
+                using (Pen pen = new Pen(Color.Red, 1))
+                {
+                    e.Graphics.DrawRectangle(pen, rectangle);
+                }
+            }
+        }
+    }
+}
